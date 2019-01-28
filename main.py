897df801b8b23d1df34272223852a89e67af80ea
Ammar <ammar@h3ll.local>
2019-01-28 00:51:14 -0500

    fixed command

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 if __name__ == '__main__':
 	base = "http://boxstarter.org/package/nr/url?"
 	print("[-] 01/28/2019 Demo - Ammar Al-Kahfah")
-	script_url = input("[~] Enter BoxStarter Script URL: ").strip()
+	script_url = raw_input("[~] Enter BoxStarter Script URL: ").strip()
 	command = base + script_url
 	print(command)
 	os.system("START {}".format(command))
